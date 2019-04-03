2a4c735f0165e68a8685ec1fb33184f19584c245
530453506 <530453506@163.com>
2019-04-03 14:56:03 +0800

    - updated

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -8,9 +8,5 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-#define PACKET_OBFUSCATION_KEY1 0x5ED10A48
-#define PACKET_OBFUSCATION_KEY2 0x667F4301
-#define PACKET_OBFUSCATION_KEY3 0x2E5D761F
-
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
